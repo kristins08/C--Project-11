51701ced4bf0c4f1c85f7c2edb47f43917086809
kristins08 <118137912+kristins08@users.noreply.github.com>
2024-08-29 11:21:26 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,14 +8,10 @@ int main()
 {
     cout << "Enter number of elements followed by the values:\n";
     
-    // DO NOT MODIFY THIS CODE!!!!
     int size;
     cin >> size;
     double prices[size];
     copy_n(istream_iterator<double>(cin), size, prices);
-    // END DO NOT MODIFY
-    // TODO Calculate Average of the array `prices' of length `size'
-    //Your code goes Here
     
     double sum=0;
     
